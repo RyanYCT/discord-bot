8c9e9d1449409c50dcddbcf7523d242f38819f50
Ryan Yip <yipchuntung@outlook.com>
2024-09-04 16:23:09 +0800

    Add .env.example and update run.bat script
    - Add .env.example to provide a template for environment variables
    - Modify run.bat to improve script functionality and ensure proper environment setup

--- run.bat
+++ run.bat
@@ -1,12 +1,31 @@
 @echo off
+REM Check if the virtual environment directory exists
+if not exist ".venv\Scripts" (
+    echo Virtual environment not found. Creating a new one...
+    python -m venv .venv
+)
+
 REM Change to the directory where your virtual environment is located
 cd .venv\Scripts
 
 REM Activate the virtual environment
 call activate
 
-REM Change to the directory where your main.py is located
+REM Change to the root directory of your project
 cd ..\..\
 
-REM Run the main.py script
-python main.py
+REM Check if requirements.txt exists
+if exist "requirements.txt" (
+    echo Installing requirements from requirements.txt...
+    pip install -r requirements.txt
+) else (
+    echo requirements.txt not found.
+)
+
+REM Check if main.py exists
+if exist "main.py" (
+    echo Running main.py...
+    python main.py
+) else (
+    echo main.py not found.
+)
